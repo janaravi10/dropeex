97fbea9be7c04fcf873935dec280cc88ee4a533a
janaravi10 <janaravi962@gmail.com>
2019-08-15 20:57:30 +0530

    Implemented the api

--- index.js
+++ index.js
@@ -1,23 +1,41 @@
 const express = require("express"),
   fs = require("fs"),
   app = express(),
-  port = process.env.PORT || 3000;
-
+  path = require("path"),
+  port = process.env.PORT || 3000,
+  base64Img = require("base64-img"),
+  uniqid = require("uniqid");
+app.use("/static", express.static(path.join(__dirname, "image")));
+app.use(express.json({ limit: "50mb" }));
 app.post("/uploadImage", (req, res) => {
-    
-  if (!fs.existsSync(__dirname + "/hello")) {
-    fs.mkdir(__dirname + "/hello", err => {
-      if (err) throw err;
-      var data = "New File Contents";
-
-      fs.writeFile(__dirname + "/hello/temp.txt", data, err => {
-        if (err) console.log(err);
-        res.send("Successfully Written to File.");
-      });
-    });
-  }else{
-      res.send("FOLDER EXIST")
+  if (!fs.existsSync(__dirname + "/image/" + req.body.image.productId)) {
+    fs.mkdirSync(__dirname + "/image/" + req.body.image.productId);
+  } else {
+    deleteFolderRecursive(__dirname + "/image/" + req.body.image.productId);
   }
+  //   req.body.image.forEach(element => {});
+  var object = { default: [] };
+  req.body.image.default.forEach(element => {
+    object.default.push(
+      base64Img.imgSync(
+        element,
+        __dirname + "/image/" + req.body.image.productId,
+        uniqid() + ""
+      )
+    );
+  });
+  object.variants = req.body.image.variants.map(element => {
+    var objName = {};
+    for (const key in element) {
+      objName[key] = base64Img.imgSync(
+        element[key],
+        __dirname + "/image/" + req.body.image.productId,
+        uniqid() + ""
+      );
+    }
+    return objName;
+  });
+  res.send(object);
 });
 app.get("/getImage", (req, res) => {
   fs.readFile(__dirname + "/hello/temp.txt", "utf-8", (err, data) => {
@@ -29,3 +47,18 @@ app.get("/getImage", (req, res) => {
   });
 });
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+function deleteFolderRecursive(path) {
+  if (fs.existsSync(path)) {
+    fs.readdirSync(path).forEach(function(file, index) {
+      var curPath = path + "/" + file;
+      if (fs.lstatSync(curPath).isDirectory()) {
+        // recurse
+        deleteFolderRecursive(curPath);
+      } else {
+        // delete file
+        fs.unlinkSync(curPath);
+      }
+    });
+    fs.rmdirSync(path);
+  }
+}
