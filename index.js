72f2824672679da2002dbfa75badead42200aa9a
janaravi10 <janaravi962@gmail.com>
2019-08-15 19:04:27 +0530

    Changed port accessibility

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express"),
   fs = require("fs"),
   app = express(),
-  port = 3000;
+  port = process.env.PORT||3000;
 
 app.post("/uploadImage", (req, res) => {
   fs.mkdir(__dirname + "/hello", err => {
