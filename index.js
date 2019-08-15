bd9945f8d8bd64d44956d00147daa4db5078a83e
janaravi10 <janaravi962@gmail.com>
2019-08-15 19:28:07 +0530

    Added some file access

--- index.js
+++ index.js
@@ -1,13 +1,31 @@
 const express = require("express"),
   fs = require("fs"),
   app = express(),
-  port = process.env.PORT||3000;
+  port = process.env.PORT || 3000;
 
 app.post("/uploadImage", (req, res) => {
-  fs.mkdir(__dirname + "/hello", err => {
-    if (err) throw err;
-    res.send("Hello world");
+    
+  if (!fs.existsSync(__dirname + "/hello")) {
+    fs.mkdir(__dirname + "/hello", err => {
+      if (err) throw err;
+      var data = "New File Contents";
+
+      fs.writeFile(__dirname + "/hello/temp.txt", data, err => {
+        if (err) console.log(err);
+        res.send("Successfully Written to File.");
+      });
+    });
+  }else{
+      res.send("FOLDER EXIST")
+  }
+});
+app.get("/getImage", (req, res) => {
+  fs.readFile(__dirname + "/hello/temp.txt", "utf-8", (err, data) => {
+    if (err) {
+      console.log(err);
+    }
+    res.send(data);
+    console.log(data);
   });
 });
-
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
